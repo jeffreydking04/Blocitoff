1167c208e70bd96526a932478630af0a06628422
Jeffrey D King <jdking33@aol.com>
2016-06-06 19:41:52 +0000

    test done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_initialize :set_default_role
+
+  def standard?
+    role == 'standard'
+  end
+
+  def premium?
+    role == 'premium'
+  end
+
+  def admin?
+    role == 'admin'
+  end
+
+  private
+
+  def set_default_role
+    self.role = 'standard'
+  end
 end
